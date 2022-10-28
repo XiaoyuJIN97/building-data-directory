6b553f14a76d154cf6cdd3d6cb2f301376d0ef32
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2022-10-28 15:53:01 +0800

    Update google_sheet_demo.py

--- pages/google_sheet_demo.py
+++ pages/google_sheet_demo.py
@@ -10,29 +10,32 @@ from pandas import DataFrame
 
 from gspread_pandas import Spread,Client
 from google.oauth2 import service_account
-
 import ssl 
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # Create a Google Authentication connection object
-scope = ['https://spreadsheets.google.com/feeds',
+scope = ["https://www.googleapis.com/auth/spreadsheets",
          'https://www.googleapis.com/auth/drive']
 
 credentials = service_account.Credentials.from_service_account_info(
-                st.secrets["gcp_service_account"], scopes = scope)
+    st.secrets["gcp_service_account"],
+    scopes=scope)
+
+#%%
 client = Client(scope=scope,creds=credentials)
 
-spreadsheetname = "Energy Dataset Intro List 1017"
+spreadsheetname = "Energy_Dataset_Intro_List_1017"
 spread = Spread(spreadsheetname,client = client)
 
+#%%
 # Check the connection
 st.write(spread.url)
 
 sh = client.open(spreadsheetname)
 worksheet_list = sh.worksheets()
-
 # Functions 
-@st.cache(ttl=100)
+@st.cache()
 # Get our worksheet names
 def worksheet_names():
     sheet_names = []   
@@ -51,14 +54,14 @@ def update_the_spreadsheet(spreadsheetname,dataframe):
     col = ['Compound CID','Time_stamp']
     spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
     st.sidebar.info('Updated to GoogleSheet')
-    
+#%%  
 st.header('Building Energy and Water Data')
-# Check whether the sheets exists
-what_sheets = worksheet_names()
-#st.sidebar.write(what_sheets)
-ws_choice = st.sidebar.radio('Available worksheets',what_sheets)
-# Load data from worksheets
-df = load_the_spreadsheet(ws_choice)
+what_sheets = 'Aggregation'
+worksheet = sh.worksheet(what_sheets)
+#%%
+#Load the datasets
+df = DataFrame(worksheet.get_all_records(head=3))
+#%%
 # Show the availibility as selection
 select_CID = st.sidebar.selectbox('Name',list(df['Dataset Full Name']))
 
