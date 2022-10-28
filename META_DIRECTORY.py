e1bdc4f3bbbb3afc593deea7e0205532cb783eb4
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2022-10-28 11:57:43 +0800

    Add files via upload

---
+++ META_DIRECTORY.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 13:56:34 2022
+
+@author: 靳笑宇
+"""
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Meta Directory",
+    #page_icon="👋",
+)
+
+st.write("# Building Data Directory")
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    A comprehensive buildig dataset collection
+""")
+
+st.image("https://github.com/XiaoyuJIN97/building-data-directory/raw/master/meta%20directory.png")
