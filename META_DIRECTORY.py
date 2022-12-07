1b9c87040deb6490b1893798e5f23e477abdb276
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2022-12-07 20:23:51 +0800

    Add files via upload

--- META_DIRECTORY.py
+++ META_DIRECTORY.py
@@ -77,6 +77,8 @@ for i in range(len(dataset)):
 dataset.index = dataset.index + 1 
 users_html = dataset.iloc[:,:-1].to_html(escape=False)
 st.write(users_html, unsafe_allow_html=True)
+
+st.markdown("""*Any questions? Contact us📧: xiaoyu.jin@connect.polyu.hk*""")
 #%%
 def update_the_spreadsheet(spreadsheetname,dataframe):
     #col = ['Dataset Full Name','URL']
